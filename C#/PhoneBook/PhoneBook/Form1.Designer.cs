a58d652b56314128ae0e6b40696fb6a2509f16b3
JamesDuBois1994 <jrd88@uakron.edu>
2016-01-28 19:27:22 -0500

    First Commit

---
+++ C#/PhoneBook/PhoneBook/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace PhoneBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameListBox = new System.Windows.Forms.ListBox();
+            this.lblPhoneNumber = new System.Windows.Forms.Label();
+            this.phoneNumLabel = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameListBox
+            // 
+            this.nameListBox.FormattingEnabled = true;
+            this.nameListBox.ItemHeight = 16;
+            this.nameListBox.Location = new System.Drawing.Point(48, 49);
+            this.nameListBox.Name = "nameListBox";
+            this.nameListBox.Size = new System.Drawing.Size(193, 228);
+            this.nameListBox.TabIndex = 0;
+            this.nameListBox.SelectedIndexChanged += new System.EventHandler(this.nameListBox_SelectedIndexChanged);
+            // 
+            // lblPhoneNumber
+            // 
+            this.lblPhoneNumber.AutoSize = true;
+            this.lblPhoneNumber.Location = new System.Drawing.Point(325, 49);
+            this.lblPhoneNumber.Name = "lblPhoneNumber";
+            this.lblPhoneNumber.Size = new System.Drawing.Size(103, 17);
+            this.lblPhoneNumber.TabIndex = 1;
+            this.lblPhoneNumber.Text = "Phone Number";
+            // 
+            // phoneNumLabel
+            // 
+            this.phoneNumLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.phoneNumLabel.Location = new System.Drawing.Point(328, 80);
+            this.phoneNumLabel.Name = "phoneNumLabel";
+            this.phoneNumLabel.Size = new System.Drawing.Size(191, 41);
+            this.phoneNumLabel.TabIndex = 2;
+            this.phoneNumLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(328, 154);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(191, 66);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select A Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 353);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.phoneNumLabel);
+            this.Controls.Add(this.lblPhoneNumber);
+            this.Controls.Add(this.nameListBox);
+            this.Name = "Form1";
+            this.Text = "PhoneBook";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox nameListBox;
+        private System.Windows.Forms.Label lblPhoneNumber;
+        private System.Windows.Forms.Label phoneNumLabel;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ C#/PhoneBook/PhoneBook/Form1.cs
@@ -0,0 +1,111 @@
+﻿// Project: PhoneBook
+// Filename: Form1.cs
+// Author: James DuBois
+// Date: 1/28/2016
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PhoneBook
+{
+    // The C# structure
+    struct PhonebookEntry
+    {
+        public string name;
+        public string phone;
+    }
+
+   
+
+
+    public partial class Form1 : Form
+    {
+
+        // Field to hold a list of PhonebookEntry objects
+        private List<PhonebookEntry> phoneList = new List<PhonebookEntry>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        // The Readfile method reads the contents of the PhoneList.txt 
+        private void ReadFile()
+        {
+            try {
+                StreamReader inputFile; // to read the file
+                string line; // to hold a line from the file
+
+                // create an instance of PhonebookEntry structre
+                PhonebookEntry entry = new PhonebookEntry();
+                // create a delimiter array
+                char[] delim = { ',' };
+                // Open the Phonelist file
+                inputFile = File.OpenText("PhoneList.txt");
+                // Read the line from the file
+                while (!inputFile.EndOfStream)
+                {
+                    // read a line from the file
+                    line = inputFile.ReadLine();
+                    // tokenize the line
+                    string[] tokens = line.Split(delim);
+                    // Store the tokens in entry object
+                    entry.name = tokens[0];
+                    entry.phone = tokens[1];
+                    // add the entry object to the list
+                    phoneList.Add(entry);
+               } // end while
+
+            } // end try
+            catch (Exception e)
+            {
+                // Display an error message
+                MessageBox.Show(e.Message);
+            }
+        } // end Readfile
+
+        // The DisplayName method displays the list of names
+        // nameListBox control
+
+            private void DisplayNames()
+        {
+            foreach (PhonebookEntry entry in phoneList)
+            {
+                nameListBox.Items.Add(entry.name);
+
+            } 
+            
+        } // end DisplayNames()
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // read the phoneList.txt file
+            ReadFile();
+            // Display the name
+            DisplayNames();
+        } // end form1_Load()
+
+        private void nameListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Get the index of the selected item
+            int index = nameListBox.SelectedIndex;
+            // Display the corresponding phone number
+            phoneNumLabel.Text = phoneList[index].phone;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
