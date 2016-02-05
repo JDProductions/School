0fc8afec46158d91d30a9b58a376109cd4cfeafd
JamesDuBois1994 <jrd88@uakron.edu>
2016-02-04 21:09:32 -0500

    Deleted JaggedArayExam - Updated

---
+++ C#/JaggedArrayExamScores/JaggedArrayExamScores/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArrayExamScores
+{
+    class Program
+    {
+        public static  void ReadFile()
+        {
+            int[][] jaggedArray = new int[3][];
+            jaggedArray[0] = new int[12]; // Section 1
+            jaggedArray[1] = new int[8];  // Section 2
+            jaggedArray[2] = new int[10];  // Section 3
+
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader("Section1.txt"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    var line = sr.ReadToEnd();
+                    var lines = sr.ReadToEnd().Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                    //Console.WriteLine(line);
+                    for (int row = 0; row < jaggedArray.Length; row++)
+                        {
+                        
+                        Console.WriteLine(jaggedArray[0][0] = line[row]);
+                        var test = " "; // Breakpoint
+                        for (int col = 0; col < jaggedArray[row].Length; col++)
+                             {
+                            //Console.WriteLine(jaggedArray[row][col].ToString());
+                            
+                             }
+                         }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ReadFile();
+        }
+    }
+}
--- C#/JaggedArrayExamScores/JaggedArrayExamScores/Properties/AssemblyInfo.cs
+++ C#/JaggedArrayExamScores/JaggedArrayExamScores/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JaggedArraysExamScores")]
+[assembly: AssemblyTitle("JaggedArrayExamScores")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JaggedArraysExamScores")]
+[assembly: AssemblyProduct("JaggedArrayExamScores")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("daed3173-2216-449a-a717-a66605941fcf")]
+[assembly: Guid("785978f9-114c-4458-b5e9-0e0057bb28d1")]
 
 // Version information for an assembly consists of the following four values:
 //
