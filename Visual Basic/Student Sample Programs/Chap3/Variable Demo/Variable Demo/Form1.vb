aaf99ffcbfef7bdb24921632f92af31b29cf9929
JamesDuBois1994 <jrd88@uakron.edu>
2016-02-09 14:20:49 -0500

    Visual Basic 3-6 Homework Done

--- Visual Basic/Student Sample Programs/Chap3/Variable Demo/Variable Demo/Form1.vb
+++ Visual Basic/Student Sample Programs/Chap3/Variable Demo/Variable Demo/Form1.vb
@@ -1,3 +1,35 @@
 ﻿Public Class Form1
+    Private Sub btnShowName_Click(sender As Object, e As EventArgs) Handles btnShowName.Click
+        Dim strFullName As String
+        ' Combine the first and last names and assign the
+        ' result to strFullName.
+        strFullName = txtFirstName.Text & " " & txtLastName.Text
+        ' Display the full name in the lblFullName
+        lblFullName.Text = strFullName
+    End Sub
 
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Clear TextBox controls and the Label.
+        txtFirstName.Clear()
+        txtLastName.Clear()
+        lblFullName.Text = String.Empty
+        ' Set focus to txtfirstName.
+        txtFirstName.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtFirstName_TextChanged(sender As Object, e As EventArgs) Handles txtFirstName.TextChanged
+
+    End Sub
+
+    Private Sub txtLastName_TextChanged(sender As Object, e As EventArgs) Handles txtLastName.TextChanged
+
+    End Sub
+
+    Private Sub lblFullName_Click(sender As Object, e As EventArgs) Handles lblFullName.Click
+
+    End Sub
 End Class
