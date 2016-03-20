86de28e3179666443becfbe42d0a0a9f42baea90
JamesDuBois1994 <jamesdubois12@gmail.com>
2016-03-20 17:13:35 -0400

    Test2 Programming Problems Complete

---
+++ Visual Basic/Lights/Lights/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub btnSwitchLight_Click(sender As Object, e As EventArgs) Handles btnSwitchLight.Click
+ If lblLightState.Text = "Off" Then
+            lblLightState.Text = "On"
+            picLightOn.Visible = True
+            picLightOff.Visible = False
+        Else
+            lblLightState.Text = "Off"
+            picLightOn.Visible = False
+            picLightOff.Visible = True
+        End If
+        
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
