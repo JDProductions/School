6875d5dc0b49bdef68143bf0629461101c21a8ba
JamesDuBois1994 <jamesdubois@oeconnection.com>
2016-04-14 11:22:48 -0400

    sdfsafsaf

---
+++ Visual Basic/StudentTestScores/StudentTestScores/Form1.Designer.vb
@@ -0,0 +1,513 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtName1 = New System.Windows.Forms.TextBox()
+        Me.txtName2 = New System.Windows.Forms.TextBox()
+        Me.txtName3 = New System.Windows.Forms.TextBox()
+        Me.txtName4 = New System.Windows.Forms.TextBox()
+        Me.txtName5 = New System.Windows.Forms.TextBox()
+        Me.txtName6 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtS11 = New System.Windows.Forms.TextBox()
+        Me.txtS12 = New System.Windows.Forms.TextBox()
+        Me.txtS13 = New System.Windows.Forms.TextBox()
+        Me.txtS14 = New System.Windows.Forms.TextBox()
+        Me.txtS15 = New System.Windows.Forms.TextBox()
+        Me.txtS25 = New System.Windows.Forms.TextBox()
+        Me.txtS24 = New System.Windows.Forms.TextBox()
+        Me.txtS23 = New System.Windows.Forms.TextBox()
+        Me.txtS22 = New System.Windows.Forms.TextBox()
+        Me.txtS21 = New System.Windows.Forms.TextBox()
+        Me.txtS35 = New System.Windows.Forms.TextBox()
+        Me.txtS34 = New System.Windows.Forms.TextBox()
+        Me.txtS33 = New System.Windows.Forms.TextBox()
+        Me.txtS32 = New System.Windows.Forms.TextBox()
+        Me.txtS31 = New System.Windows.Forms.TextBox()
+        Me.txtS45 = New System.Windows.Forms.TextBox()
+        Me.txtS44 = New System.Windows.Forms.TextBox()
+        Me.txtS43 = New System.Windows.Forms.TextBox()
+        Me.txtS42 = New System.Windows.Forms.TextBox()
+        Me.txtS41 = New System.Windows.Forms.TextBox()
+        Me.txtS55 = New System.Windows.Forms.TextBox()
+        Me.txtS54 = New System.Windows.Forms.TextBox()
+        Me.txtS53 = New System.Windows.Forms.TextBox()
+        Me.txtS52 = New System.Windows.Forms.TextBox()
+        Me.txtS51 = New System.Windows.Forms.TextBox()
+        Me.txtS65 = New System.Windows.Forms.TextBox()
+        Me.txtS64 = New System.Windows.Forms.TextBox()
+        Me.txtS63 = New System.Windows.Forms.TextBox()
+        Me.txtS62 = New System.Windows.Forms.TextBox()
+        Me.txtS61 = New System.Windows.Forms.TextBox()
+        Me.lblAvg1 = New System.Windows.Forms.Label()
+        Me.lblAvg2 = New System.Windows.Forms.Label()
+        Me.lblAvg3 = New System.Windows.Forms.Label()
+        Me.lblAvg4 = New System.Windows.Forms.Label()
+        Me.lblAvg5 = New System.Windows.Forms.Label()
+        Me.lblAvg6 = New System.Windows.Forms.Label()
+        Me.btnCalculateAverage = New System.Windows.Forms.Button()
+        Me.SuspendLayout
+        '
+        'txtName1
+        '
+        Me.txtName1.Location = New System.Drawing.Point(12, 39)
+        Me.txtName1.Name = "txtName1"
+        Me.txtName1.Size = New System.Drawing.Size(129, 20)
+        Me.txtName1.TabIndex = 0
+        '
+        'txtName2
+        '
+        Me.txtName2.Location = New System.Drawing.Point(12, 76)
+        Me.txtName2.Name = "txtName2"
+        Me.txtName2.Size = New System.Drawing.Size(129, 20)
+        Me.txtName2.TabIndex = 1
+        '
+        'txtName3
+        '
+        Me.txtName3.Location = New System.Drawing.Point(12, 113)
+        Me.txtName3.Name = "txtName3"
+        Me.txtName3.Size = New System.Drawing.Size(129, 20)
+        Me.txtName3.TabIndex = 2
+        '
+        'txtName4
+        '
+        Me.txtName4.Location = New System.Drawing.Point(12, 154)
+        Me.txtName4.Name = "txtName4"
+        Me.txtName4.Size = New System.Drawing.Size(129, 20)
+        Me.txtName4.TabIndex = 3
+        '
+        'txtName5
+        '
+        Me.txtName5.Location = New System.Drawing.Point(12, 193)
+        Me.txtName5.Name = "txtName5"
+        Me.txtName5.Size = New System.Drawing.Size(129, 20)
+        Me.txtName5.TabIndex = 4
+        '
+        'txtName6
+        '
+        Me.txtName6.Location = New System.Drawing.Point(12, 233)
+        Me.txtName6.Name = "txtName6"
+        Me.txtName6.Size = New System.Drawing.Size(129, 20)
+        Me.txtName6.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.Location = New System.Drawing.Point(12, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = true
+        Me.Label2.Location = New System.Drawing.Point(236, 23)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Test Scores"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = true
+        Me.Label3.Location = New System.Drawing.Point(460, 23)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Average"
+        '
+        'txtS11
+        '
+        Me.txtS11.Location = New System.Drawing.Point(170, 39)
+        Me.txtS11.Name = "txtS11"
+        Me.txtS11.Size = New System.Drawing.Size(36, 20)
+        Me.txtS11.TabIndex = 9
+        '
+        'txtS12
+        '
+        Me.txtS12.Location = New System.Drawing.Point(212, 39)
+        Me.txtS12.Name = "txtS12"
+        Me.txtS12.Size = New System.Drawing.Size(36, 20)
+        Me.txtS12.TabIndex = 10
+        '
+        'txtS13
+        '
+        Me.txtS13.Location = New System.Drawing.Point(254, 39)
+        Me.txtS13.Name = "txtS13"
+        Me.txtS13.Size = New System.Drawing.Size(36, 20)
+        Me.txtS13.TabIndex = 11
+        '
+        'txtS14
+        '
+        Me.txtS14.Location = New System.Drawing.Point(296, 39)
+        Me.txtS14.Name = "txtS14"
+        Me.txtS14.Size = New System.Drawing.Size(36, 20)
+        Me.txtS14.TabIndex = 12
+        '
+        'txtS15
+        '
+        Me.txtS15.Location = New System.Drawing.Point(338, 39)
+        Me.txtS15.Name = "txtS15"
+        Me.txtS15.Size = New System.Drawing.Size(36, 20)
+        Me.txtS15.TabIndex = 13
+        '
+        'txtS25
+        '
+        Me.txtS25.Location = New System.Drawing.Point(338, 76)
+        Me.txtS25.Name = "txtS25"
+        Me.txtS25.Size = New System.Drawing.Size(36, 20)
+        Me.txtS25.TabIndex = 18
+        '
+        'txtS24
+        '
+        Me.txtS24.Location = New System.Drawing.Point(296, 76)
+        Me.txtS24.Name = "txtS24"
+        Me.txtS24.Size = New System.Drawing.Size(36, 20)
+        Me.txtS24.TabIndex = 17
+        '
+        'txtS23
+        '
+        Me.txtS23.Location = New System.Drawing.Point(254, 76)
+        Me.txtS23.Name = "txtS23"
+        Me.txtS23.Size = New System.Drawing.Size(36, 20)
+        Me.txtS23.TabIndex = 16
+        '
+        'txtS22
+        '
+        Me.txtS22.Location = New System.Drawing.Point(212, 76)
+        Me.txtS22.Name = "txtS22"
+        Me.txtS22.Size = New System.Drawing.Size(36, 20)
+        Me.txtS22.TabIndex = 15
+        '
+        'txtS21
+        '
+        Me.txtS21.Location = New System.Drawing.Point(170, 76)
+        Me.txtS21.Name = "txtS21"
+        Me.txtS21.Size = New System.Drawing.Size(36, 20)
+        Me.txtS21.TabIndex = 14
+        '
+        'txtS35
+        '
+        Me.txtS35.Location = New System.Drawing.Point(338, 113)
+        Me.txtS35.Name = "txtS35"
+        Me.txtS35.Size = New System.Drawing.Size(36, 20)
+        Me.txtS35.TabIndex = 23
+        '
+        'txtS34
+        '
+        Me.txtS34.Location = New System.Drawing.Point(296, 113)
+        Me.txtS34.Name = "txtS34"
+        Me.txtS34.Size = New System.Drawing.Size(36, 20)
+        Me.txtS34.TabIndex = 22
+        '
+        'txtS33
+        '
+        Me.txtS33.Location = New System.Drawing.Point(254, 113)
+        Me.txtS33.Name = "txtS33"
+        Me.txtS33.Size = New System.Drawing.Size(36, 20)
+        Me.txtS33.TabIndex = 21
+        '
+        'txtS32
+        '
+        Me.txtS32.Location = New System.Drawing.Point(212, 113)
+        Me.txtS32.Name = "txtS32"
+        Me.txtS32.Size = New System.Drawing.Size(36, 20)
+        Me.txtS32.TabIndex = 20
+        '
+        'txtS31
+        '
+        Me.txtS31.Location = New System.Drawing.Point(170, 113)
+        Me.txtS31.Name = "txtS31"
+        Me.txtS31.Size = New System.Drawing.Size(36, 20)
+        Me.txtS31.TabIndex = 19
+        '
+        'txtS45
+        '
+        Me.txtS45.Location = New System.Drawing.Point(338, 154)
+        Me.txtS45.Name = "txtS45"
+        Me.txtS45.Size = New System.Drawing.Size(36, 20)
+        Me.txtS45.TabIndex = 28
+        '
+        'txtS44
+        '
+        Me.txtS44.Location = New System.Drawing.Point(296, 154)
+        Me.txtS44.Name = "txtS44"
+        Me.txtS44.Size = New System.Drawing.Size(36, 20)
+        Me.txtS44.TabIndex = 27
+        '
+        'txtS43
+        '
+        Me.txtS43.Location = New System.Drawing.Point(254, 154)
+        Me.txtS43.Name = "txtS43"
+        Me.txtS43.Size = New System.Drawing.Size(36, 20)
+        Me.txtS43.TabIndex = 26
+        '
+        'txtS42
+        '
+        Me.txtS42.Location = New System.Drawing.Point(212, 154)
+        Me.txtS42.Name = "txtS42"
+        Me.txtS42.Size = New System.Drawing.Size(36, 20)
+        Me.txtS42.TabIndex = 25
+        '
+        'txtS41
+        '
+        Me.txtS41.Location = New System.Drawing.Point(170, 154)
+        Me.txtS41.Name = "txtS41"
+        Me.txtS41.Size = New System.Drawing.Size(36, 20)
+        Me.txtS41.TabIndex = 24
+        '
+        'txtS55
+        '
+        Me.txtS55.Location = New System.Drawing.Point(338, 193)
+        Me.txtS55.Name = "txtS55"
+        Me.txtS55.Size = New System.Drawing.Size(36, 20)
+        Me.txtS55.TabIndex = 33
+        '
+        'txtS54
+        '
+        Me.txtS54.Location = New System.Drawing.Point(296, 193)
+        Me.txtS54.Name = "txtS54"
+        Me.txtS54.Size = New System.Drawing.Size(36, 20)
+        Me.txtS54.TabIndex = 32
+        '
+        'txtS53
+        '
+        Me.txtS53.Location = New System.Drawing.Point(254, 193)
+        Me.txtS53.Name = "txtS53"
+        Me.txtS53.Size = New System.Drawing.Size(36, 20)
+        Me.txtS53.TabIndex = 31
+        '
+        'txtS52
+        '
+        Me.txtS52.Location = New System.Drawing.Point(212, 193)
+        Me.txtS52.Name = "txtS52"
+        Me.txtS52.Size = New System.Drawing.Size(36, 20)
+        Me.txtS52.TabIndex = 30
+        '
+        'txtS51
+        '
+        Me.txtS51.Location = New System.Drawing.Point(170, 193)
+        Me.txtS51.Name = "txtS51"
+        Me.txtS51.Size = New System.Drawing.Size(36, 20)
+        Me.txtS51.TabIndex = 29
+        '
+        'txtS65
+        '
+        Me.txtS65.Location = New System.Drawing.Point(338, 233)
+        Me.txtS65.Name = "txtS65"
+        Me.txtS65.Size = New System.Drawing.Size(36, 20)
+        Me.txtS65.TabIndex = 38
+        '
+        'txtS64
+        '
+        Me.txtS64.Location = New System.Drawing.Point(296, 233)
+        Me.txtS64.Name = "txtS64"
+        Me.txtS64.Size = New System.Drawing.Size(36, 20)
+        Me.txtS64.TabIndex = 37
+        '
+        'txtS63
+        '
+        Me.txtS63.Location = New System.Drawing.Point(254, 233)
+        Me.txtS63.Name = "txtS63"
+        Me.txtS63.Size = New System.Drawing.Size(36, 20)
+        Me.txtS63.TabIndex = 36
+        '
+        'txtS62
+        '
+        Me.txtS62.Location = New System.Drawing.Point(212, 233)
+        Me.txtS62.Name = "txtS62"
+        Me.txtS62.Size = New System.Drawing.Size(36, 20)
+        Me.txtS62.TabIndex = 35
+        '
+        'txtS61
+        '
+        Me.txtS61.Location = New System.Drawing.Point(170, 233)
+        Me.txtS61.Name = "txtS61"
+        Me.txtS61.Size = New System.Drawing.Size(36, 20)
+        Me.txtS61.TabIndex = 34
+        '
+        'lblAvg1
+        '
+        Me.lblAvg1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAvg1.Location = New System.Drawing.Point(463, 41)
+        Me.lblAvg1.Name = "lblAvg1"
+        Me.lblAvg1.Size = New System.Drawing.Size(62, 23)
+        Me.lblAvg1.TabIndex = 39
+        '
+        'lblAvg2
+        '
+        Me.lblAvg2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAvg2.Location = New System.Drawing.Point(463, 73)
+        Me.lblAvg2.Name = "lblAvg2"
+        Me.lblAvg2.Size = New System.Drawing.Size(62, 23)
+        Me.lblAvg2.TabIndex = 40
+        '
+        'lblAvg3
+        '
+        Me.lblAvg3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAvg3.Location = New System.Drawing.Point(463, 115)
+        Me.lblAvg3.Name = "lblAvg3"
+        Me.lblAvg3.Size = New System.Drawing.Size(62, 23)
+        Me.lblAvg3.TabIndex = 41
+        '
+        'lblAvg4
+        '
+        Me.lblAvg4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAvg4.Location = New System.Drawing.Point(463, 156)
+        Me.lblAvg4.Name = "lblAvg4"
+        Me.lblAvg4.Size = New System.Drawing.Size(62, 23)
+        Me.lblAvg4.TabIndex = 42
+        '
+        'lblAvg5
+        '
+        Me.lblAvg5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAvg5.Location = New System.Drawing.Point(463, 195)
+        Me.lblAvg5.Name = "lblAvg5"
+        Me.lblAvg5.Size = New System.Drawing.Size(62, 23)
+        Me.lblAvg5.TabIndex = 43
+        '
+        'lblAvg6
+        '
+        Me.lblAvg6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAvg6.Location = New System.Drawing.Point(463, 233)
+        Me.lblAvg6.Name = "lblAvg6"
+        Me.lblAvg6.Size = New System.Drawing.Size(62, 23)
+        Me.lblAvg6.TabIndex = 44
+        '
+        'btnCalculateAverage
+        '
+        Me.btnCalculateAverage.Location = New System.Drawing.Point(441, 286)
+        Me.btnCalculateAverage.Name = "btnCalculateAverage"
+        Me.btnCalculateAverage.Size = New System.Drawing.Size(115, 57)
+        Me.btnCalculateAverage.TabIndex = 45
+        Me.btnCalculateAverage.Text = "Calculat Average"
+        Me.btnCalculateAverage.UseVisualStyleBackColor = true
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(602, 364)
+        Me.Controls.Add(Me.btnCalculateAverage)
+        Me.Controls.Add(Me.lblAvg6)
+        Me.Controls.Add(Me.lblAvg5)
+        Me.Controls.Add(Me.lblAvg4)
+        Me.Controls.Add(Me.lblAvg3)
+        Me.Controls.Add(Me.lblAvg2)
+        Me.Controls.Add(Me.lblAvg1)
+        Me.Controls.Add(Me.txtS65)
+        Me.Controls.Add(Me.txtS64)
+        Me.Controls.Add(Me.txtS63)
+        Me.Controls.Add(Me.txtS62)
+        Me.Controls.Add(Me.txtS61)
+        Me.Controls.Add(Me.txtS55)
+        Me.Controls.Add(Me.txtS54)
+        Me.Controls.Add(Me.txtS53)
+        Me.Controls.Add(Me.txtS52)
+        Me.Controls.Add(Me.txtS51)
+        Me.Controls.Add(Me.txtS45)
+        Me.Controls.Add(Me.txtS44)
+        Me.Controls.Add(Me.txtS43)
+        Me.Controls.Add(Me.txtS42)
+        Me.Controls.Add(Me.txtS41)
+        Me.Controls.Add(Me.txtS35)
+        Me.Controls.Add(Me.txtS34)
+        Me.Controls.Add(Me.txtS33)
+        Me.Controls.Add(Me.txtS32)
+        Me.Controls.Add(Me.txtS31)
+        Me.Controls.Add(Me.txtS25)
+        Me.Controls.Add(Me.txtS24)
+        Me.Controls.Add(Me.txtS23)
+        Me.Controls.Add(Me.txtS22)
+        Me.Controls.Add(Me.txtS21)
+        Me.Controls.Add(Me.txtS15)
+        Me.Controls.Add(Me.txtS14)
+        Me.Controls.Add(Me.txtS13)
+        Me.Controls.Add(Me.txtS12)
+        Me.Controls.Add(Me.txtS11)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtName6)
+        Me.Controls.Add(Me.txtName5)
+        Me.Controls.Add(Me.txtName4)
+        Me.Controls.Add(Me.txtName3)
+        Me.Controls.Add(Me.txtName2)
+        Me.Controls.Add(Me.txtName1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents txtName1 As TextBox
+    Friend WithEvents txtName2 As TextBox
+    Friend WithEvents txtName3 As TextBox
+    Friend WithEvents txtName4 As TextBox
+    Friend WithEvents txtName5 As TextBox
+    Friend WithEvents txtName6 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtS11 As TextBox
+    Friend WithEvents txtS12 As TextBox
+    Friend WithEvents txtS13 As TextBox
+    Friend WithEvents txtS14 As TextBox
+    Friend WithEvents txtS15 As TextBox
+    Friend WithEvents txtS25 As TextBox
+    Friend WithEvents txtS24 As TextBox
+    Friend WithEvents txtS23 As TextBox
+    Friend WithEvents txtS22 As TextBox
+    Friend WithEvents txtS21 As TextBox
+    Friend WithEvents txtS35 As TextBox
+    Friend WithEvents txtS34 As TextBox
+    Friend WithEvents txtS33 As TextBox
+    Friend WithEvents txtS32 As TextBox
+    Friend WithEvents txtS31 As TextBox
+    Friend WithEvents txtS45 As TextBox
+    Friend WithEvents txtS44 As TextBox
+    Friend WithEvents txtS43 As TextBox
+    Friend WithEvents txtS42 As TextBox
+    Friend WithEvents txtS41 As TextBox
+    Friend WithEvents txtS55 As TextBox
+    Friend WithEvents txtS54 As TextBox
+    Friend WithEvents txtS53 As TextBox
+    Friend WithEvents txtS52 As TextBox
+    Friend WithEvents txtS51 As TextBox
+    Friend WithEvents txtS65 As TextBox
+    Friend WithEvents txtS64 As TextBox
+    Friend WithEvents txtS63 As TextBox
+    Friend WithEvents txtS62 As TextBox
+    Friend WithEvents txtS61 As TextBox
+    Friend WithEvents lblAvg1 As Label
+    Friend WithEvents lblAvg2 As Label
+    Friend WithEvents lblAvg3 As Label
+    Friend WithEvents lblAvg4 As Label
+    Friend WithEvents lblAvg5 As Label
+    Friend WithEvents lblAvg6 As Label
+    Friend WithEvents btnCalculateAverage As Button
+End Class
---
+++ Visual Basic/StudentTestScores/StudentTestScores/Form1.vb
@@ -0,0 +1,58 @@
+﻿Module StudentTestScoresModule
+
+    Const intMAX_SUBSCRIPT_STUDENT As Integer = 6
+    Const intMAX_SUBSCRIPT_STUDENT_SCORES As Integer = 5
+   'create structure
+    Public Structure StudentData
+       Dim strName As String
+       Dim dblTestScoresArray() As Double
+       Dim dblAverage As Double
+    End Structure
+
+Dim dblTotalStd1 As Double
+    Dim dblTotalStd2 As Double
+Dim dblScore As Double
+
+Dim StudentsArray(intMAX_SUBSCRIPT_STUDENT) As StudentData
+
+Sub StudentNameDataInput()
+    StudentsArray(0).strName = Form1.txtName1.Text
+    StudentsArray(1).strName = Form1.txtName2.Text
+    StudentsArray(2).strName = Form1.txtName3.Text
+    StudentsArray(3).strName = Form1.txtName4.Text
+    StudentsArray(4).strName = Form1.txtName5.Text
+    StudentsArray(5).strName = Form1.txtName6.Text
+End Sub
+
+Sub StudentScoreDataInput()
+    For intIndex = 0 To intMAX_SUBSCRIPT_STUDENT
+        ReDim StudentsArray(intIndex).dblTestScoresArray(4)
+    Next
+    'test scores for first student
+    StudentsArray(0).dblTestScoresArray(0) = CDbl(Form1.txtS11.Text)
+    StudentsArray(1).dblTestScoresArray(1) = CDbl(Form1.txtS12.Text)
+    StudentsArray(2).dblTestScoresArray(2) = CDbl(Form1.txtS13.Text)
+    StudentsArray(3).dblTestScoresArray(3) = CDbl(Form1.txtS14.Text)
+    StudentsArray(4).dblTestScoresArray(4) = CDbl(Form1.txtS15.Text)
+
+    For Each i As StudentData In StudentsArray
+    For Each S as Double in i.dblTestScoresArray
+        dblTotalStd1 += s
+    Next
+Next
+    Dim dblAverage As Double = dblTotalStd1/intMAX_SUBSCRIPT_STUDENT_SCORES
+    Form1.lblAvg1.Text = (dblAverage.ToString)
+End Sub
+
+Sub CalculateAverage()
+End Sub
+
+End Module
+
+ Public Class Form1
+
+
+Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculateAverage.Click
+    StudentScoreDataInput()
+End Sub
+End Class
